0de5273e48e3b579e9fc3c01b39369a51c3c17ca
alexoviedo999 <alexoviedo999@gmail.com>
2013-02-25 14:48:50 -0500

    added user model added index model added followers views added index routes added instagram-node; mongo/mongoose packages. added cookie and session middleware

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,54 @@ ig.use({
 module.exports.create = function (app) {
   app.get('/', function (req, res) {
     res.render('index', {
-      title: "test"
+      title: "home"
     });
   });
 
   app.get('/explore', function (req, res, next){
     ig.media_popular(function(err, medias, limit) {
-      res.json(medias);
+      res.render('explore', {
+        title: 'Exlore Page',
+        medias: medias
+      });
+    });
+  });
+
+  app.get('/location/:latitude/:longitude', function (req, res, next) {
+    var lat = Number(req.param('latitude'))
+    var lng = Number(req.param('longitude'))
+
+  ig.location_search({ lat: lat, lng: lng }, function(err, result, limit) {
+      if (err) {
+        console.log(err);
+        return next(err);
+      }
+
+      if (result.length == 0) {
+        return res.send('empty');
+      }
+
+      var location = result[0]
+
+      ig.location_media_recent(location.id, function(err, result, pagination, limit) {
+  if (err) {
+    console.log(err);
+    return next(err);
+  }
+
+        console.log(require('util').inspect(result));
+        console.log(require('util').inspect(limit));
+
+        res.render('location', {
+          title: 'location',
+          medias: result,
+          location: location
+        });
+      });
     });
   });
 }
+
+
+
+
